bdc20b16dc8afbfbf37c3580f5b93df63a3ba375
nelson shoby <nelsonshoby009@gmail.com>
2021-11-29 17:17:31 +0530

    Added unit tests to article and category model

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -3,7 +3,29 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @article = build(:article)
+  end
+
+  def test_article_is_valid
+    assert @article.valid?
+  end
+
+  def test_article_should_not_be_valid_and_saved_without_title
+    @article.title = " "
+    assert_not @article.valid?
+    assert_includes @article.errors.full_messages, "Title can't be blank"
+  end
+
+  def test_article_should_not_be_valid_and_saved_without_content
+    @article.content = " "
+    assert_not @article.valid?
+    assert_includes @article.errors.full_messages, "Content can't be blank"
+  end
+
+  def test_article_should_not_be_valid_and_saved_without_status
+    @article.status = " "
+    assert_not @article.valid?
+    assert_includes @article.errors.full_messages, "Status can't be blank"
+  end
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -3,7 +3,17 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @category = build(:category)
+  end
+
+  def test_category_is_valid
+    assert @category.valid?
+  end
+
+  def test_category_should_not_be_valid_and_saved_without_name
+    @category.name = " "
+    assert_not @category.valid?
+    assert_includes @category.errors.full_messages, "Name can't be blank"
+  end
 end
