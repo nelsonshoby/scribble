45df1709a1b2355e88a42843ecae875801d46dd7
nelson shoby <nelsonshoby009@gmail.com>
2021-11-30 13:16:24 +0530

    Added subheader

--- app/javascript/src/components/Dashboard/index.jsx
+++ app/javascript/src/components/Dashboard/index.jsx
@@ -2,12 +2,18 @@ import React from "react";
 
 import Menubar from "./Menubar";
 import Navbar from "./Navbar";
+import Subheading from "./Subheading";
 
 const Dashboard = () => {
   return (
     <div>
       <Navbar />
-      <Menubar />
+      <div className="flex">
+        <Menubar />
+        <div className="m-4">
+          <Subheading />
+        </div>
+      </div>
     </div>
   );
 };
