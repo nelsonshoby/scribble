3eb854895769195e1c623d06c25c68cffb6510b9
nelson shoby <nelsonshoby009@gmail.com>
2021-12-08 19:14:10 +0530

    Completed front end part for drag and drop functionality

--- app/javascript/src/components/Settings/ManageCategories/index.jsx
+++ app/javascript/src/components/Settings/ManageCategories/index.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Plus, Reorder, Edit, Delete } from "@bigbinary/neeto-icons";
 import { Typography, Button } from "@bigbinary/neetoui/v2";
 import Logger from "js-logger";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { toast } from "react-toastify";
 
 import CreateOrEditCategory from "./CreateOrEditCategory";
@@ -15,6 +16,7 @@ const ManageCategories = () => {
   const [editableId, setEditableId] = useState();
   const [name, setName] = useState("");
   const [newCategory, setNewCategory] = useState(true);
+  const [characters, updateCharacters] = useState();
 
   const handleUpdate = async id => {
     try {
@@ -60,6 +62,8 @@ const ManageCategories = () => {
       const response = await categoryApi.index();
       Logger.warn("response in cat", response.data.category);
       setCategoryList(response.data.category);
+      Logger.warn("categoryList", categoryList);
+      updateCharacters(response.data.category);
       setEditableId(null);
     } catch (error) {
       Logger.error(error);
@@ -69,9 +73,18 @@ const ManageCategories = () => {
   useEffect(() => {
     ListCategories();
   }, []);
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+
+    const items = Array.from(characters);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    updateCharacters(items);
+  }
 
   return (
-    <div className="mx-auto overflow-scroll ">
+    <div className="mx-auto  ">
       <div className="w-720 mt-4">
         <Typography style="h2">Manage Categories</Typography>
         <Typography style="body1" className="text-gray-600 mt-2">
@@ -102,45 +115,79 @@ const ManageCategories = () => {
           )}
         </div>
         <div className="w-641">
-          <ul>
-            {categoryList?.map(category =>
-              editableId !== category.id ? (
-                <li className="flex justify-between  border-t py-4 items-center">
-                  <div className="flex  items-center w-2/4">
-                    <Button icon={Reorder} style="text" className="mr-2" />
-                    <Typography style="h4">{category.name}</Typography>
-                  </div>
-                  <div className="flex">
-                    <Button
-                      className="mr-2 "
-                      icon={Edit}
-                      style="text"
-                      onClick={() => {
-                        setEditableId(category.id);
-                        setName(category.name);
-                        setNewCategory(true);
-                      }}
-                    />
-                    <Button
-                      icon={Delete}
-                      style="text"
-                      onClick={() => handleDelete(category.id)}
-                    />
-                  </div>
-                </li>
-              ) : (
-                <div className="border-t">
-                  <CreateOrEditCategory
-                    setEditableId={setEditableId}
-                    handleSubmit={handleUpdate}
-                    setName={setName}
-                    name={name}
-                    editableId={editableId}
-                  />
-                </div>
-              )
-            )}
-          </ul>
+          <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable droppableId="characters">
+              {provided => (
+                <ul
+                  className="characters"
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  {characters?.map((category, index) => {
+                    return (
+                      <Draggable
+                        key={String(category.id)}
+                        draggableId={String(category.id)}
+                        index={index}
+                      >
+                        {provided =>
+                          editableId !== category.id ? (
+                            <li
+                              className="flex justify-between  border-t py-4 items-center"
+                              key={category.id}
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              <div className="flex  items-center w-2/4">
+                                <Button
+                                  icon={Reorder}
+                                  style="text"
+                                  className="mr-2"
+                                />
+                                <Typography style="h4">
+                                  {category.name}
+                                </Typography>
+                              </div>
+                              <div className="flex">
+                                <Button
+                                  className="mr-2 "
+                                  icon={Edit}
+                                  style="text"
+                                  onClick={() => {
+                                    setEditableId(category.id);
+                                    setName(category.name);
+                                    setNewCategory(true);
+                                  }}
+                                />
+                                <Button
+                                  icon={Delete}
+                                  style="text"
+                                  onClick={() => handleDelete(category.id)}
+                                />
+                              </div>
+                            </li>
+                          ) : (
+                            <div className="border-t">
+                              <CreateOrEditCategory
+                                setEditableId={setEditableId}
+                                handleSubmit={handleUpdate}
+                                setName={setName}
+                                name={name}
+                                editableId={editableId}
+                              />
+                            </div>
+                          )
+                        }
+                      </Draggable>
+                    );
+                  })}
+
+                  {provided.placeholder}
+                </ul>
+              )}
+            </Droppable>
+          </DragDropContext>
         </div>
       </div>
     </div>
