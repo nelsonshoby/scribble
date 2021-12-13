d17565ac422ba72c1261a6dfff839fa4ee221b11
nelson shoby <nelsonshoby009@gmail.com>
2021-12-14 02:32:22 +0530

    Added unit test to redirection model

--- test/models/redirection_test.rb
+++ test/models/redirection_test.rb
@@ -4,18 +4,16 @@ require "test_helper"
 
 class RedirectionTest < ActiveSupport::TestCase
   def setup
-    @redirection = Redirection.new(From: "/", To: "/preview")
+    @redirection = build(:redirection)
+    redirection2 = Redirection.create(From: "Home", To: "About")
   end
 
-  def test_redirection_should_be_invalid_without_from
-    @redirection.From = ""
-    assert @redirection.invalid?
-    assert_includes @redirection.errors.full_messages, "From can't be blank"
+  def test_redirection_should_be_valid
+    assert @redirection.valid?
   end
 
-  def test_redirection_should_be_invalid_without_to
-    @redirection.To = ""
+  def test_redirection_should_be_invalid_for_duplicate_from
+    @redirection.From = "Home"
     assert @redirection.invalid?
-    assert_includes @redirection.errors.full_messages, "To can't be blank"
   end
 end
